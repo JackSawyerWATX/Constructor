26aba6c538b8fabce4facaf93900870e64e9b5ca
Jack Sawyer <84543199+JackSawyerWATX@users.noreply.github.com>
2022-09-01 10:24:52 -0700

    first commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+public class Main { // Create a Main class
+    String x; // The Attribute
+
+    // The constructor for the Main class
+    public Main() {
+        x = "Jon"; // this is the value for the class attribute
+    }
+
+    public static void main(String[] args) {
+        Main name = new Main(); // This is the object of class Main (This is what calls the constructor)
+        System.out.println(name.x); // Print statement
+    }
+}
